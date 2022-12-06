813772bad21f8f2db01121915aab736be3079245
João Pster <joaopsterdev@gmail.com>
2022-12-06 02:58:42 -0300

    feat(pages): Atualizada e Finalizada a Página Home e Login

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
 
 import AppWrap from "../../constants/wrapper/AppWrap";
 import { Header, Footer } from "../../components";
+import { CarCard } from "../../components/CarCard";
+
+import { GlobalContext } from "../../context/Global";
+import { requestGet } from "../../utils/Request";
+
+import { ICarroWithId } from "../../constants/interfaces/Carro";
 
 import "./Home.scss";
 
 function Home() {
-  return <div>Home</div>;
+  const [carros, setCarros] = useState<ICarroWithId[]>([]);
+
+  const {
+    loading: { setLoading },
+  } = useContext(GlobalContext);
+
+  useEffect(() => {
+    setLoading(true);
+
+    async function getCars() {
+      console.log("Main!");
+      const response = await requestGet("/api/v1/carros");
+      if (response) {
+        const carrosOrdenados = response.sort(
+          (a: ICarroWithId, b: ICarroWithId) => {
+            return Number(b.valor_original) - Number(a.valor_original);
+          }
+        );
+
+        setCarros(carrosOrdenados);
+      }
+    }
+
+    getCars();
+    setLoading(false);
+  }, []);
+
+  return (
+    <div className="app__home">
+      {carros &&
+        carros.map((car: ICarroWithId) => <CarCard car={car} key={car.id} />)}
+    </div>
+  );
 }
 
 export default AppWrap(Home, Header, Footer);
--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,9 +1,129 @@
-import React from "react";
+import * as React from "react";
+import { useNavigate } from "react-router-dom";
 
-import "./Login.scss";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
-function Login() {
-  return <div>Login</div>;
+import { requestPost } from "../../utils/Request";
+import { GlobalContext } from "../../context/Global";
+
+export default function SignIn() {
+  const {
+    login: { login },
+  } = React.useContext(GlobalContext);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    const userLogin = {
+      username: data.get("username"),
+      password: data.get("password"),
+    };
+
+    const response = await requestPost("/api/v1/token/", userLogin);
+    if (!response) alert("Usuário ou senha inválidos");
+
+    const userData = {
+      name: "João Pster",
+      username: "joaopster",
+      email: "joaopsterdev@gmail.com",
+      token: response.access,
+    };
+
+    await login(userData);
+    navigate("/admin");
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "primary" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Painel Administrativo
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Nome de Usuário"
+            placeholder="joaopster"
+            name="username"
+            autoComplete="username"
+            color="secondary"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Senha"
+            placeholder="%joaopster2022%"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            color="secondary"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="secondary" />}
+            label="Lembrar credenciais"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            color="secondary"
+          >
+            Entrar
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link
+                href="http://localhost:3000/login"
+                variant="body2"
+                color="secondary"
+              >
+                Perdeu a Senha?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link
+                href="http://localhost:3000/login"
+                variant="body2"
+                color="secondary"
+              >
+                Não tem um conta? Registre-se
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
-
-export default Login;
