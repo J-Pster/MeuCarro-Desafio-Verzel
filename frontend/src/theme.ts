cf1879df78de2282f4bf6afba10d49cc210b5ec2
João Pster <joaopsterdev@gmail.com>
2022-12-06 02:59:05 -0300

    style(theme): Novos padrões de cores no theme do MUI

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -5,10 +5,13 @@ import { red } from "@mui/material/colors";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#556cd6",
+      main: "#ffee32",
     },
     secondary: {
-      main: "#19857b",
+      main: "#202020",
+    },
+    background: {
+      default: "#f5f3f4",
     },
     error: {
       main: red.A400,
